76d38c02cffd1254df6c4b0e280436866ffcbdce
KaminK <k.kvvnkm@gmail.com>
2026-04-14 16:53:13 +0800

    build driver (abstraction) for user to use publish and subscribe features to monitor data on plc

---
+++ SYS8.Core/Driver/SYS8Monitoring.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using SYS8.Core.PubSub;
+
+namespace SYS8.Core.Driver
+{
+    public class SYS8Monitoring
+    {
+        private readonly PublishAndSubscribeModel _model;
+
+        public Action<string, object>? OnValueChanged
+        {
+            get => _model.OnValueChanged;
+            set => _model.OnValueChanged = value;
+        }
+
+        public bool IsPolling => _model.IsPolling;
+        public bool IsEmpty => _model.IsEmpty;
+
+        public SYS8Monitoring(SYS8Driver driver)
+        {
+            _model = new PublishAndSubscribeModel(driver);
+        }
+
+        public Task Subscribe(string topic, string datatype)
+            => _model.Subscribe(topic, datatype);
+
+        public void Unsubscribe(string topic)
+            => _model.Unsubscribe(topic);
+
+        public void StartPolling(int interval = 1000)
+            => _model.StartPolling(interval);
+
+        public void StopPolling()
+            => _model.StopPolling();
+
+        public List<string> GetSubscribedTopics()
+            => _model.GetSubscribedTopics();
+    }
+}
