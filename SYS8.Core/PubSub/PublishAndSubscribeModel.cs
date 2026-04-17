c154c61715f24770a002a3c30372d09610283a2e
KaminK <k.kvvnkm@gmail.com>
2026-04-17 15:37:55 +0800

    feat: publish and subscribe for boolean array. Reorganize the data structure for subscription into another fiel named SubscriptionItems

--- SYS8.Core/PubSub/PublishAndSubscribeModel.cs
+++ SYS8.Core/PubSub/PublishAndSubscribeModel.cs
@@ -12,13 +12,14 @@ namespace SYS8.Core.PubSub
     internal class PublishAndSubscribeModel
     {
         private readonly SYS8Driver _driver;
-        private Dictionary<string, string> _datatype = new();
-        private Dictionary<string, object> _previousValues = new();
+        //private Dictionary<string, string> _datatype = new();
+        //private Dictionary<string, object> _previousValues = new();
+        private readonly Dictionary<string, SubscriptionItem> _subscriptions = new();
         private CancellationTokenSource? _pollingCts;
 
         public Action<string, object>? OnValueChanged { get; set; }
 
-        public bool IsEmpty => _previousValues.Count == 0;
+        public bool IsEmpty => _subscriptions.Count == 0;
         public bool IsPolling => _pollingCts != null;
 
         public PublishAndSubscribeModel(SYS8Driver driver)
@@ -76,7 +77,7 @@ namespace SYS8.Core.PubSub
 
         public List<string> GetSubscribedTopics()
         {
-            return _previousValues.Keys.ToList();
+            return _subscriptions.Keys.ToList();
         }
 
 
@@ -97,13 +98,15 @@ namespace SYS8.Core.PubSub
                 throw new InvalidOperationException("Driver is not connected.");
             }
 
-            if(_previousValues.ContainsKey(topic)) 
+            if(_subscriptions.ContainsKey(topic)) 
             {
                 throw new InvalidOperationException($"Topic '{topic}' is already subscribed.");
             }
 
-            _datatype[topic] = datatype.ToLower(); 
-            object initialValue =  datatype.ToLower() switch {
+            var (dbNumber, byteOffset, bitIndex) = ParseStringAddress(topic);
+            string normalizedType = datatype.ToLower();
+
+            object initialValue =  normalizedType switch {
                 "bool" => await _driver.ReadBoolAsync(topic),
                 "int16" => await _driver.ReadInt16Async(topic),
                 "uint16" => await _driver.ReadUInt16Async(topic),
@@ -114,15 +117,30 @@ namespace SYS8.Core.PubSub
                 "string" => string.Empty,
                 _ => throw new ArgumentException($"Unsupported data type: {datatype}")
             };
-            _previousValues[topic] = initialValue;
+            _subscriptions[topic] = new SubscriptionItem
+            {
+                Topic = topic,
+                DataType = normalizedType,
+                PreviousValue = initialValue,
+                DbNumber = dbNumber,
+                ByteOffset = byteOffset,
+                BitIndex = bitIndex,
+                IsArrayElement = false,
+                ArrayRootTopic = null,
+                ArrayIndex = -1,
+                ArrayLength = 0
+            };
         }
 
-        public async Task<string> SubscribeArray(string? topic, uint length, string? datatype) // , Action<string, object> callback
+
+        public async Task<string> SubscribeArray(string? topic, int length, string? datatype) // , Action<string, object> callback
         {
             if (string.IsNullOrEmpty(topic) || string.IsNullOrEmpty(datatype))
             {
                 throw new ArgumentException("Topic and datatype cannot be null or empty.");
             }
+
+            //there is checking if length is > 0 inside Read Method
 
 
             if (!_driver.IsConnected)
@@ -130,38 +148,65 @@ namespace SYS8.Core.PubSub
                 throw new InvalidOperationException("Driver is not connected.");
             }
 
+            var normalizedType = datatype.ToLower();
             var (dbNumber, byteOffset, bitIndex) = ParseStringAddress(topic);
 
-            int initialOffset = byteOffset * 8 + bitIndex;
-            int localByteOffset = byteOffset ;
-            int localBitIndex = bitIndex;
-            for (int i = initialOffset; i < initialOffset + length; i++)
-            { 
-                localByteOffset = i / 8;
-                localBitIndex = i % 8;
+            object[] returnValueArray;
+            switch (datatype.ToLower())
+            {
+                case "bool":
+                    bool[] boolArray = await _driver.ReadBoolArrayAsync(topic, length);
+                    returnValueArray = boolArray.Cast<object>().ToArray();
+                    break;
+                default:
+                    throw new ArgumentException($"Unsupported data type: {datatype}");
+            }
+
+            string lastTopic = topic;
+
+            for (int i = 0; i < length; i++)
+            {
+                int absoluteBitOffset = byteOffset * 8 + bitIndex + i;
+                int localByteOffset = absoluteBitOffset / 8;
+                int localBitIndex = absoluteBitOffset % 8;
+
                 string tempTopic = ConvertToAbsoluteAddress(dbNumber, localByteOffset, localBitIndex);
 
-                if (_previousValues.ContainsKey(tempTopic))
+                if (_subscriptions.ContainsKey(tempTopic))
                 {
                     throw new InvalidOperationException($"Topic '{tempTopic}' is already subscribed.");
                 }
 
-                _datatype[tempTopic] = datatype.ToLower();
-                object initialValue = datatype.ToLower() switch
+                _subscriptions[tempTopic] = new SubscriptionItem
                 {
-                    "bool" => await _driver.ReadBoolAsync(dbNumber, localByteOffset, localBitIndex),
-                    "int16" => await _driver.ReadInt16Async(dbNumber, localByteOffset, localBitIndex),
-                    "uint16" => await _driver.ReadUInt16Async(dbNumber, localByteOffset, localBitIndex),
-                    "int32" => await _driver.ReadInt32Async(dbNumber, localByteOffset, localBitIndex),
-                    "uint32" => await _driver.ReadUInt32Async(dbNumber, localByteOffset, localBitIndex),
-                    "float32" => await _driver.ReadFloat32Async(dbNumber, localByteOffset, localBitIndex),
-                    "float64" => await _driver.ReadFloat64Async(dbNumber, localByteOffset, localBitIndex),
-                    "string" => string.Empty,
-                    _ => throw new ArgumentException($"Unsupported data type: {datatype}")
+                    Topic = tempTopic,
+                    DataType = normalizedType,
+                    PreviousValue = returnValueArray[i],
+                    DbNumber = dbNumber,
+                    ByteOffset = localByteOffset,
+                    BitIndex = localBitIndex,
+                    IsArrayElement = true,
+                    ArrayRootTopic = topic,
+                    ArrayIndex = i,
+                    ArrayLength = length
                 };
-                _previousValues[tempTopic] = initialValue;
+                lastTopic = tempTopic;
+
+                //object initialValue = datatype.ToLower() switch
+                //{
+                //    "bool" => await _driver.ReadBoolAsync(dbNumber, localByteOffset, localBitIndex),
+                //    "int16" => await _driver.ReadInt16Async(dbNumber, localByteOffset, localBitIndex),
+                //    "uint16" => await _driver.ReadUInt16Async(dbNumber, localByteOffset, localBitIndex),
+                //    "int32" => await _driver.ReadInt32Async(dbNumber, localByteOffset, localBitIndex),
+                //    "uint32" => await _driver.ReadUInt32Async(dbNumber, localByteOffset, localBitIndex),
+                //    "float32" => await _driver.ReadFloat32Async(dbNumber, localByteOffset, localBitIndex),
+                //    "float64" => await _driver.ReadFloat64Async(dbNumber, localByteOffset, localBitIndex),
+                //    "string" => string.Empty,
+                //    _ => throw new ArgumentException($"Unsupported data type: {datatype}")
+                //};
+                //_previousValues[tempTopic] = initialValue;
             }
-            return ConvertToAbsoluteAddress(dbNumber, localByteOffset, localBitIndex); //last topic in the array, can be used for unsubscribing the whole array later 
+            return lastTopic; //last topic in the array, can be used for unsubscribing the whole array later 
         }
 
 
@@ -173,12 +218,11 @@ namespace SYS8.Core.PubSub
             {
                 throw new InvalidOperationException("Driver is not connected.");
             }
-            if (!_previousValues.ContainsKey(topic))
+            if (!_subscriptions.ContainsKey(topic))
             {
                 throw new InvalidOperationException($"Topic '{topic}' is not subscribed.");
             }
-            _datatype.Remove(topic);
-            _previousValues.Remove(topic);
+            _subscriptions.Remove(topic);
         }
 
         public async Task CheckForUpdates()
@@ -189,9 +233,41 @@ namespace SYS8.Core.PubSub
             {
                 throw new InvalidOperationException("Driver is not connected.");
             }
-            foreach (var topic in _previousValues.Keys.ToList()) //make a copy of keys to avoid modification during iteration
+
+            var processedTopics = new HashSet<string>();
+
+            var boolArrayGroups = _subscriptions.Values
+                .Where(s => s.DataType == "bool" && s.IsArrayElement && !string.IsNullOrEmpty(s.ArrayRootTopic))
+                .GroupBy(s => s.ArrayRootTopic);
+
+            foreach (var group in boolArrayGroups)
             {
-                object newValue = _datatype[topic] switch
+                string rootTopic = group.Key!;
+                int length = group.Max(x => x.ArrayLength ?? 0);
+
+                bool[] newValues = await _driver.ReadBoolArrayAsync(rootTopic, length);
+
+                foreach (var sub in group)
+                {
+                    bool newValue = newValues[sub.ArrayIndex];
+                    if (!Equals(newValue, sub.PreviousValue))
+                    {
+                        sub.PreviousValue = newValue;
+                        OnValueChanged?.Invoke(sub.Topic, newValue);
+                    }
+
+                    processedTopics.Add(sub.Topic);
+                }
+            }
+
+            foreach (var topic in _subscriptions.Keys.ToList())
+            {
+                if (processedTopics.Contains(topic))
+                    continue;
+
+                var sub = _subscriptions[topic];
+
+                object newValue = sub.DataType switch
                 {
                     "bool" => await _driver.ReadBoolAsync(topic),
                     "int16" => await _driver.ReadInt16Async(topic),
@@ -201,16 +277,13 @@ namespace SYS8.Core.PubSub
                     "float32" => await _driver.ReadFloat32Async(topic),
                     "float64" => await _driver.ReadFloat64Async(topic),
                     "string" => string.Empty,
-                    _ => throw new ArgumentException($"Unsupported data type: {_datatype[topic]}")
+                    _ => throw new ArgumentException($"Unsupported data type: {sub.DataType}")
                 };
-                if (!Equals(newValue, _previousValues[topic]))
+
+                if (!Equals(newValue, sub.PreviousValue))
                 {
-                    // Value has changed, invoke callbacks here (not implemented in this snippet)
-                    // For example: InvokeCallbacks(topic, newValue);
-                    // Update previous value
-                    _previousValues[topic] = newValue;
+                    sub.PreviousValue = newValue;
                     OnValueChanged?.Invoke(topic, newValue);
-                   
                 }
             }
         }
---
+++ SYS8.Core/PubSub/SubscriptionItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SYS8.Core.PubSub
+{
+    internal class SubscriptionItem
+    {
+        public string Topic { get; set; } = string.Empty;
+        public string DataType { get; set; } = string.Empty;
+        public object? PreviousValue { get; set; }
+
+        public ushort DbNumber { get; set; }
+        public int ByteOffset { get; set; }
+        public int BitIndex { get; set; }
+
+        public bool IsArrayElement { get; set; }
+        public string? ArrayRootTopic { get; set; }
+        public string? ArrayEndingTopic { get; set; }
+        public int ArrayIndex { get; set; }
+
+        public int? ArrayLength { get; set; }
+    }
+}
